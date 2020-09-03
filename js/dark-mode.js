cce475e17a2a150f58501ca664380ed433ddce68
Roman <pomankrentovsky@gmail.com>
2020-09-03 16:43:05 +0300

    add dark mode

---
+++ js/dark-mode.js
@@ -0,0 +1,10 @@
+(function () {
+  const options = {
+    cursor: 'pointer',
+    time: '0.5s', // default: '0.3s'
+    label: '🌓', // default: ''
+  };
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+})();
