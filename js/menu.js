93aafba78b1fc2f4bf91665a72d1afe9ed18e22f
Roman <pomankrentovsky@gmail.com>
2020-09-17 12:43:43 +0300

    fix focus

--- js/menu.js
+++ js/menu.js
@@ -3,23 +3,42 @@
     menu = document.querySelector('.header__nav'),
     body = document.querySelector('body'),
     OPEN_MENU_CLASS = 'active',
-    OVERFLOW__BODY = 'overflow';
+    OVERFLOW__BODY = 'overflow',
+    inputAfterMenu = document.querySelector('.main-screen__input');
 
   const showCloseMenu = () => {
     menuIcon.querySelector('svg').classList.toggle(OPEN_MENU_CLASS);
     menu.classList.toggle(OPEN_MENU_CLASS);
     body.classList.toggle(OVERFLOW__BODY);
+    if (menu.classList.contains(OPEN_MENU_CLASS)) {
+      menu.querySelector('.header__link').focus();
+    }
+    menu.querySelector('.search__close').addEventListener('keydown', function (evt) {
+      if (evt.code === 'Tab') {
+        evt.stopPropagation();
+        menuIcon.focus();
+      }
+    });
   };
 
-  menuIcon.addEventListener('click', function (evt) {
-    showCloseMenu(evt);
-  });
-
   const showCloseMenuClick = (evt) => {
-    if (evt.code === 'Enter') {
+    evt.stopPropagation();
+    if (evt.code === 'Enter' || evt.code === 'Escape') {
       showCloseMenu();
+    } else if (evt.shiftKey && evt.keyCode == 9) {
+      menu.querySelector('.search__icon').focus();
+    } else if (evt.code === 'Tab') {
+      inputAfterMenu.focus();
     }
   };
 
+  menuIcon.addEventListener('click', (evt) => showCloseMenu(evt));
   menuIcon.addEventListener('keydown', (evt) => showCloseMenuClick(evt));
+
+  inputAfterMenu.addEventListener('keydown', function (evt) {
+    if (evt.shiftKey && evt.keyCode == 9) {
+      evt.stopPropagation();
+      menuIcon.focus();
+    }
+  });
 })();
--- js/search-input.js
+++ js/search-input.js
@@ -1,31 +1,41 @@
 (function () {
   const searchBlock = document.querySelector('.search');
+  const searchInput = document.querySelector('.search__input');
   const searchOpenIcon = searchBlock.querySelector('.search__icon');
   const searchCloseIcon = searchBlock.querySelector('.search__close');
+  const inputAfterMenu = document.querySelector('.main-screen__input');
+  const MOBILE_WIDTH = 1200;
 
   const showSearch = () => {
     searchBlock.style.overflow = 'visible';
     searchOpenIcon.classList.add('hidden');
     searchOpenIcon.nextSibling.nextSibling.classList.add('active');
     searchCloseIcon.classList.add('active');
+    searchInput.focus();
   };
 
   const closeSearch = () => {
     searchBlock.style.overflow = 'hidden';
     searchCloseIcon.classList.remove('active');
-    searchOpenIcon.classList.remove('hidden');
     searchOpenIcon.nextSibling.nextSibling.classList.remove('active');
+    searchOpenIcon.classList.remove('hidden');
   };
 
   const showSearchClick = (evt) => {
     if (evt.code === 'Enter') {
       showSearch();
+    } else if (evt.code === 'Tab' && window.innerWidth >= MOBILE_WIDTH) {
+      inputAfterMenu.focus();
+    } else if (evt.code === 'Tab' && window.innerWidth < MOBILE_WIDTH) {
+      document.querySelector('.burger').focus();
     }
   };
 
   const closeSearchClick = (evt) => {
     if (evt.code === 'Enter') {
       closeSearch();
+    } else if (evt.shiftKey && evt.keyCode == 9) {
+      searchOpenIcon.focus();
     }
   };
 
